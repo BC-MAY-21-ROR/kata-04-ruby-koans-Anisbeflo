a8818af05d18d58deac905134a415e6f0d484328
Anisbeflo <abejarano0@ucol.mx>
2021-12-12 15:58:23 -0600

    Progress 62%
    - about_blocks.rb done
    - about_exceptions.rb done
    - about_iteration.rb done
    - about_triangle_project.rb done
    - about_triangle_project_2.rb done
    - triangle.rb: created code for checking isosceles, equilateral and scalene. And errors verifications.

--- koans/triangle.rb
+++ koans/triangle.rb
@@ -14,6 +14,13 @@
 #   about_triangle_project_2.rb
 #
 def triangle(a, b, c)
+  a,b,c = [a,b,c].sort
+  raise TriangleError if a<=0 || a+b<=c
+  a==b ? lado1=true : lado1=false
+  a==c ? lado2=true : lado2=false
+  b==c ? lado3=true : lado3=false
+  lado1==true && lado2==true ? :equilateral :
+  lado1==true || lado2==true || lado3==true ? :isosceles : :scalene
   # WRITE THIS CODE
 end
 
